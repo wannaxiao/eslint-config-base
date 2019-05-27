249fb4c40806f629235c74da36670ca405422faa
momoko <ko.momo@qq.com>
2019-05-27 11:54:22 +0800

    feat(ts): @typescript-eslint/explicit-function-return-type

--- packages/eslint-config-ts/index.js
+++ packages/eslint-config-ts/index.js
@@ -16,30 +16,37 @@ module.exports = {
   // 'error' 或 2 - 开启规则，使用错误级别的错误：error (当被触发的时候，程序会退出)
   rules: {
     // 自定义规则：
-    // 使用 @momoko/js 规则
-    '@typescript-eslint/indent': 'off',
-    '@typescript-eslint/no-undef': 'off',
-    '@typescript-eslint/no-unused-vars': 'off',
+    // 使用 @typescript-eslint 规则
+    'indent': 'off',
+    'no-undef': 'off',
+    'no-unused-vars': 'off',
+    // 缩进 2 空格
+    '@typescript-eslint/indent': ['error', 2],
     // 除 import 语句外，不允许使用 require 语句
     '@typescript-eslint/no-var-requires': 'off',
     // 在类属性和方法上需要显式的可访问性修饰符
     '@typescript-eslint/explicit-member-accessibility': [
       'error',
-      { accessibility: 'no-public' }
+      { 'accessibility': 'no-public' },
     ],
     // 接口和类的成员分隔符样式
     '@typescript-eslint/member-delimiter-style': [
       'error',
       {
-        multiline: {
-          delimiter: 'none',
-          requireLast: true,
+        'multiline': {
+          'delimiter': 'none',
+          'requireLast': true,
         },
-        singleline: {
-          delimiter: 'semi',
-          requireLast: false,
+        'singleline': {
+          'delimiter': 'semi',
+          'requireLast': false,
         },
       },
+    ],
+    // 只检查声明函数的返回值，不检查匿名函数/函数表达式
+    '@typescript-eslint/explicit-function-return-type': [
+      'error',
+      { 'allowExpressions': true },
     ],
   },
 }
